16ba200d0e8674c74367bf2ac8c40cf8f4468e34
Ethan <wuethann@gmail.com>
2022-09-24 13:48:07 -0400

    Add javascript practices

---
+++ practice1/javascript.js
@@ -0,0 +1,25 @@
+let name, admin;
+
+name = "John";
+
+admin = name;
+
+
+let nameOfOurPlanet;
+
+nameOfOurPlanet = "Earth";
+
+let currentUser;
+
+currentUser = "Name of Current Visitor to a website";
+
+const myInt = 5;
+
+const myFloat = 6.667;
+
+myInt;
+myFloat;
+
+typeof myInt;
+typeof myFloat;
+
